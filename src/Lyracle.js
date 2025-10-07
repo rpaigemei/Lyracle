4f562c788d9c34bcdc9b18d7bec87e497238aa74
rpaigemei <paigereeves@ufl.edu>
2025-10-07 19:39:00 -0400

    remove {AnimatePresence} for deploy

--- src/Lyracle.js
+++ src/Lyracle.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import { AnimatePresence, motion } from "framer-motion";
+import { motion } from "framer-motion";
 import { fetchTracks } from "./api/lastFM";
 import Card from "./components/card";
 import lyracle from "./assets/images/lyracleIcon.png"
